5079f78a7d641869a632ae2b3d2f44ec26b07b91
BennyChrispin <benny.ndizihiwe@amalitechtraining.org>
2024-07-26 17:40:46 +0200

    some changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,9 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
